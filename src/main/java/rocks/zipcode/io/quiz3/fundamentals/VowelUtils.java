f71f6500e45c7b8c3b2649c700adad33e8222083
kendrang <kendrang@users.noreply.github.com>
2019-11-18 14:01:15 -0500

    vowels done

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
@@ -10,28 +10,46 @@ import java.util.concurrent.LinkedBlockingDeque;
 public class VowelUtils {
 
 
-    public static  String[] vowels = {"a", "e", "i", "o", "u"};
+    public static String[] vowels = {"a", "e", "i", "o", "u"};
+
     public static Boolean hasVowels(String word) {
-
-        for (Character c : word.toCharArray()){
-//            if (c.equals(v))
+        char[] wordArr = word.toCharArray();
+        for (Character c : wordArr) {
+            if (isVowel(c) == true) {
+                return true;
+            }
         }
-
-        return null;
+        return false;
     }
 
     public static Integer getIndexOfFirstVowel(String word) {
-        return null;
+        int indexCount = 0;
+        char[] wordArr = word.toCharArray();
+        for (Character c : wordArr) {
+            if (!hasVowels(word)) {
+                return -1;
+
+            } else if (isVowel(c)) {
+                break;
+            }
+
+            indexCount++;
+
+        }
+        return indexCount;
     }
 
-
     public static Boolean startsWithVowel(String word) {
-        return null;
+        char[] wordArr = word.toCharArray();
+        if (isVowel(wordArr[0])){
+            return true;
+        }
+        return false;
     }
 
     public static Boolean isVowel(Character character) {
-        for (int i = 0 ; i < 5 ; i++){
-            if (character.toString().equalsIgnoreCase(vowels[i]) == true){
+        for (int i = 0; i < 5; i++) {
+            if (character.toString().equalsIgnoreCase(vowels[i]) == true) {
                 return true;
             }
         }
