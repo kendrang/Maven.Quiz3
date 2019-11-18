e65e0cbd419cd1d67017636a7239f6021a487caf
kendrang <kendrang@users.noreply.github.com>
2019-11-18 14:23:18 -0500

    done stringtils

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/StringUtils.java
@@ -1,25 +1,49 @@
 package rocks.zipcode.io.quiz3.fundamentals;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author leon on 09/12/2018.
  */
 public class StringUtils {
     public static String capitalizeNthCharacter(String str, Integer indexToCapitalize) {
-        return null;
+        String result = "";
+        char[] wordArr = str.toCharArray();
+     for (int i = 0 ; i < wordArr.length ; i++){
+         if (i == indexToCapitalize){
+            String capMe = String.valueOf(wordArr[i]);
+            result += capMe.toUpperCase();
+         }
+         else result += wordArr[i];
+     }
+        return result;
     }
 
     public static Boolean isCharacterAtIndex(String baseString, Character characterToCheckFor, Integer indexOfString) {
-        return null;
+        char[] wordArr = baseString.toCharArray();
+        for (int i = 0 ; i < wordArr.length ; i ++){
+            if (wordArr[indexOfString] == characterToCheckFor){
+                return true;
+            }
+        }
+        return false;
     }
 
     public static String[] getAllSubStrings(String string) {
-        return null;
+        Set<String> subStrings = new HashSet<>();
+        for (int i = 0 ; i < string.length() ; i ++){
+            for (int j = i+1 ; j <= string.length(); j++){
+               subStrings.add(string.substring(i,j));
+            }
+        }
+        return subStrings.toArray(new String[0]);
     }
 
     public static Integer getNumberOfSubStrings(String input){
-        return null;
+
+        return getAllSubStrings(input).length;
     }
 }
