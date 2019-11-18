2459dca3df244be933ffa6f7a5c476d7d686ecef
kendrang <kendrang@users.noreply.github.com>
2019-11-18 13:47:52 -0500

    one set of tests

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/VowelUtils.java
@@ -1,10 +1,22 @@
 package rocks.zipcode.io.quiz3.fundamentals;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingDeque;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class VowelUtils {
+
+
+    public static  String[] vowels = {"a", "e", "i", "o", "u"};
     public static Boolean hasVowels(String word) {
+
+        for (Character c : word.toCharArray()){
+//            if (c.equals(v))
+        }
+
         return null;
     }
 
@@ -18,6 +30,11 @@ public class VowelUtils {
     }
 
     public static Boolean isVowel(Character character) {
-        return null;
+        for (int i = 0 ; i < 5 ; i++){
+            if (character.toString().equalsIgnoreCase(vowels[i]) == true){
+                return true;
+            }
+        }
+        return false;
     }
 }
