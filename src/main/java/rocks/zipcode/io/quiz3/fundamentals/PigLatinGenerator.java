57455d12460a1af74a54ae08511ec074dd0e117f
kendrang <kendrang@users.noreply.github.com>
2019-11-18 15:18:33 -0500

    done pig latin

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -20,17 +20,22 @@ public class PigLatinGenerator extends VowelUtils {
         String result = "";
         for (int i = 0 ; i < pigWords.length ; i ++){
             char[] charA = pigWords[i].toString().toCharArray();
+
+            if (!hasVowels(pigWords[i])){
+                result += pigWords[i]+"ay";
+                return result;
+            }
             if (startsWithVowel(pigWords[i])){
-                result += pigWords[i]+"may";
+                result += pigWords[i]+"way ";
             }
             if(!startsWithVowel(pigWords[i])){
                 String newWord = "";
                newWord += pigWords[i].substring(getIndexOfFirstVowel(pigWords[i]), pigWords[i].length());
-               newWord += pigWords[i].substring(0,getIndexOfFirstVowel(pigWords[i])) + "ay";
+               newWord += pigWords[i].substring(0,getIndexOfFirstVowel(pigWords[i])) + "ay ";
                result += newWord;
             }
 
         }
-        return result;
+        return result.trim();
     }
 }
